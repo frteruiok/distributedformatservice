5499d3b5bb6412721b7caa3bb439795e36207ebf
frteruiok <dsjciegobravo.c@hotmail.com>
2024-05-11 08:27:26 +0700

    test

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function countVowels(str) {
-  return str.match(/[aeiou]/gi).length;
+function pathSum(root, sum) {
+  if (!root) return false;
+  if (!root.left && !root.right && root.val === sum) return true;
+  return (
+    pathSum(root.left, sum - root.val) || pathSum(root.right, sum - root.val)
+  );
 }
